16a5f941fdb415201ed96fded03005f259450d7c
grace1513 <g.umwiza@alustudent.com>
2026-04-18 10:51:52 -0700

    Added 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Python is an interpreted, interactive, object-oriented programming\
  language that avoids nearly all the syntactic sugar"
-print(str1[36:65] + str1[67:72] + str1[0:6])
+print(str1[36:66] + str1[74:79] + str1[0:6])
 
