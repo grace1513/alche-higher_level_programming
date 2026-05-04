8ac8c4caf6c4b7c5eb315dcb0f70dfc11f11ef4e
grace1513 <g.umwiza@alustudent.com>
2026-05-04 11:02:02 -0700

    Added 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,3 +17,24 @@ class Square:
     def size(self):
         """Retrieve the size of the square."""
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the current square area."""
+        return self.__size ** 2
