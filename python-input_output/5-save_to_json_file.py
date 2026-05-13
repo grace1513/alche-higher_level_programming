0e7df3a19795268baf75abff346758774a419830
grace1513 <g.umwiza@alustudent.com>
2026-05-13 13:25:13 -0700

    Added 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module that creates an object from a JSON file."""
+"""Module that saves an object to a file using JSON representation."""
 import json
 
 
-def load_from_json_file(filename):
-    """Create an object from a JSON file."""
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
