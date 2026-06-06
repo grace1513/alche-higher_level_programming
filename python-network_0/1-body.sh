5e0c16341b744ecf8277c83020e865e134c82e7f
grace1513 <g.umwiza@alustudent.com>
2026-06-06 06:20:20 +0000

    Added 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Sends a GET request and displays the body of a 200 response
-curl -sL "$1"
+response=$(curl -s -w "\n%{http_code}" "$1")
+body=$(echo "$response" | head -n -1)
+status=$(echo "$response" | tail -n 1)
+
+if [ "$status" -eq 200 ]; then
+    echo "$body"
+fi
