9b43797e8199ed33120a602e40037c84c09005a9
grace1513 <g.umwiza@alustudent.com>
2026-06-13 12:47:06 +0000

    Added  0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetches the status from ALU intranet and displays the response body."""
-
+"""Module that fetches https://alu-intranet.hbtn.io/status"""
 from urllib import request
 
-if **name** == "**main**":
-with request.urlopen("https://alu-intranet.hbtn.io/status") as response:
-body = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
 
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
