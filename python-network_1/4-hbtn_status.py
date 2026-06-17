1d37631f271bc75a81ae0f680c2be3d328860c78
grace1513 <g.umwiza@alustudent.com>
2026-06-17 06:43:29 +0000

    Added 4-hbtn_status.py

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches a URL using requests."""
+
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
+    body = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
