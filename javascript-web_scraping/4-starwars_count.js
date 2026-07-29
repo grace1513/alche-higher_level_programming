e01916f50dc66f68de6b4f9bd028c43ea3880f53
grace1513 <g.umwiza@alustudent.com>
2026-07-29 12:24:22 -0700

    Added 4-starwars_count.js

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  let count = 0;
+
+  films.forEach((film) => {
+    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  });
+
+  console.log(count);
+});
