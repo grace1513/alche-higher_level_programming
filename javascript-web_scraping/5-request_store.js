5acddb4f126f22f94d239e00972052207cec3c83
grace1513 <g.umwiza@alustudent.com>
2026-07-29 12:34:06 -0700

    Added 5-request_store.js

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
